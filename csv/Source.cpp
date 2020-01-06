ed8d1b29efc39dd308c0d6de6a97f3c33b68bc50
usagix <takuma.t0616@gmail.com>
2020-01-06 22:15:08 +0900

    csvの読み込み処理

---
+++ csv/Source.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <conio.h>
+
+using namespace std;
+
+vector<string> split(string& input, char delimiter)
+{
+	istringstream stream(input);
+	string field;
+	vector<string> result;
+	while (getline(stream, field, delimiter)) {
+		result.push_back(field);
+	}
+	return result;
+}
+
+int main()
+{
+	ifstream ifs("data.csv");
+
+	string line;
+	// csvファイルをの最後の行までループを回す
+	while (getline(ifs, line)) {
+
+		vector<string> strvec = split(line, ',');
+
+		for (int i = 0; i < strvec.size(); i++) {
+			printf("%5d\n", stoi(strvec.at(i)));
+		}
+
+	}
+
+	_getch();
+}
