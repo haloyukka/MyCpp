85bc46c9eaf1435065e3e2b8f2b1aa25f4db5074
haloyukka <gm.halohalo@gmail.com>
2021-11-24 08:31:35 +0900

    [ADD] anime file

---
+++ anime.cpp
@@ -0,0 +1,32 @@
+// anime.cpp : Defines the entry point for the console application.
+//
+// アスキーアニメ（鳥）
+#include <stdio.h>
+#include <windows.h>
+void  main( )
+{
+    while ( 1 )                                 // 無限ループ
+      {
+        system("cls");                          // 画面消去
+        printf("                     \n");
+        printf("              ***    \n");
+        printf("             *       \n");
+        printf("    *********        \n");
+        printf("      ******         \n");
+        printf("       *  *          \n");
+        printf("      *    *         \n");
+        printf("_y_y_y*y_y_y*y_y_y_y_\n");
+        Sleep(500);                             // 500ミリ秒止まる
+
+        system("cls");                          // 画面消去
+        printf("                     \n");
+        printf("             ***     \n");
+        printf("             *       \n");
+        printf("    *********        \n");
+        printf("      ******         \n");
+        printf("        **           \n");
+        printf("        **           \n");
+        printf("y_y_y_y_**y_y_y_y_y_y\n");
+        Sleep(500);                             // 500ミリ秒止まる
+      }
+}
